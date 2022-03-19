dbb2f99d96fac8c355d3e54fd7d493e686532647
Sam <sammccathie2@gmail.com>
2022-03-19 14:39:45 +1300

    Added header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,30 @@
 import React from "react";
+import "./Header.scss";
+import Home from "../../svgs/Home.svg";
+import Notifications from "../../svgs/Notifications.svg";
+import Profile from "../../svgs/Profile.svg";
+import XP from "../../svgs/XP.svg";
 
-export const Header = ({children}) => {
+export const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-      {children}
-    </div>
+    <nav className="header">
+      <div className="header__content">
+        <div className="header__logo">Logo</div>
+        <div className="header__buttons">
+          <div className="header__button">
+            <img src={Home} alt=""></img>
+          </div>
+          <div className="header__button">
+            <img src={Notifications} alt=""></img>
+          </div>
+          <div className="header__button">
+            <img src={Profile} alt=""></img>
+          </div>
+          <div className="header__button">
+            <img src={XP} alt=""></img>
+          </div>
+        </div>
+      </div>
+    </nav>
   );
 };
