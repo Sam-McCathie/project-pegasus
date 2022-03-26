8b8835dc0ddc7ea72c75d70acf9295a8e683175d
Sam <sammccathie2@gmail.com>
2022-03-26 18:36:18 +1300

    Array created based on current d/m/y

--- src/components/Calendar/NodeData.js
+++ src/components/Calendar/NodeData.js
@@ -1,5 +1,8 @@
 import React from "react";
 
+// Notes
+// - add a handler / test jan & dec current dates
+
 export const NodeData = () => {
   // testing array creation
   const days = [
@@ -12,47 +15,94 @@ export const NodeData = () => {
     "Saturday",
   ];
 
-  const d = new Date(); // current date
-  const D = {m: d.getMonth(), y: d.getFullYear()};
-  const daysInMonth = new Date(D.y, D.m + 1, 0).getDate(); // m -> 0 indexed?
-  const monthArr = [...Array(daysInMonth).keys()].map((i) => i + 1);
+  // todays date
+  const d = new Date();
+  console.log(d);
 
-  const firstDay = new Date(D.y, D.m, 1);
+  // todays month & year
+  const D = {m: d.getMonth() + 1, y: d.getFullYear()};
+  console.log(D);
+
+  // days in current month
+  const daysInMonth = new Date(D.y, D.m, 0).getDate(); // m -> 0 indexed?
+  console.log(daysInMonth);
+
+  // format day data
+  // - add to object identifyer if month is current? -> will help with CCS later
+  const createDate = (date, month, year) => {
+    const d = (date + 1).toString();
+    const dID = d.length > 1 ? date : `0${date}`;
+
+    const m = month.toString();
+    const mID = m.length > 1 ? m : `0${m}`;
+
+    const data = {
+      id: dID + mID + year,
+      date: date,
+      month: month,
+      year: year,
+    };
+    return data;
+  };
+
+  // array of days in current month
+  const currentMonthDays = [...Array(daysInMonth).keys()].map((i) => {
+    return createDate(i + 1, D.m, D.y);
+  });
+
+  // get first day of current month
+  const firstDay = new Date(D.y, D.m - 1, 1);
+  console.log(firstDay);
+
+  // get DOW of the first day of current month
   const first = days[firstDay.getDay()];
-  const i = days.indexOf(first);
-  const previous = new Date(D.y, D.m, 0).getDate(); // m -> 0 indexed?
-  // extract the last i amount of days in prev month
-  //   -- review daysInMonth method
+  console.log(first);
 
+  // get DOW index of first day in current month
+  const iF = days.indexOf(first) - 1;
+  console.log(iF);
+
+  // get length of previous month
+  const previous = new Date(D.y, D.m - 1, 0).getDate();
   console.log(previous);
 
-  console.log(monthArr);
+  // create an array of the extra days to round out the current month
+  let previousMonthDays = [];
+  if (iF > 0) {
+    for (let x = 0; x < iF; x++) {
+      previousMonthDays.push(createDate(previous - x, D.m - 1, D.y));
+    }
+  }
+  console.log(previousMonthDays);
 
-  // function getFirstDayOfMonth(year, month) {
-  //   return new Date(year, month, 1);
-  // }
+  // get last day of current month
+  const lastDay = new Date(D.y, D.m, 0);
+  console.log(lastDay);
 
-  // const firstDayCurrentMonth = getFirstDayOfMonth(
-  //   d.getFullYear(),
-  //   d.getMonth()
-  // );
+  // get DOW of the last day of current month
+  const last = days[lastDay.getDay()];
+  console.log(last);
 
-  // const firstDayDOW = days[firstDayCurrentMonth.getDay()];
-  // console.log(firstDayDOW);
+  // get DOW index of last day of current month
+  const iL = days.indexOf(last);
+  console.log(iL);
+
+  // create an array of the extra days to round out the current month
+  let nextMonthDays = [];
+  if (iL < days.length - 1) {
+    for (let x = 1; x <= days.length - iL; x++) {
+      nextMonthDays.push(createDate(x, D.m + 1, D.y));
+    }
+  }
+  console.log(nextMonthDays);
+
+  // current full array including extra days before current month
+  let currentMonth = [
+    ...previousMonthDays,
+    ...currentMonthDays,
+    ...nextMonthDays,
+  ];
+  console.log(currentMonth);
 
   return <div>NodeData</div>;
 };
-
-// get current month
-// get start date for current month
-// get days of month
-// create an array the length of the month with each day
-// add days from previous month based one where start day is.
-// - e.g. of start day = tuesday + 1 of last month.
-// repeat for end of the month
-
-// save day id as 250322 or 011023 etc or split up into D/M/Y db cols
-
-// create an array base on result
-// if month starts on a Wednesday grab last two days of previous month as array.
-// - add update a class to indicicate past or next month for colour changes
