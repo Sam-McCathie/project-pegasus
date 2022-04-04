367564a753a238dc51bd6fc3cddd44bbe159c779
Sam <sammccathie2@gmail.com>
2022-04-04 16:24:36 +1200

    updated to react-helmet-async, fixed key console errors

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import {Helmet} from "react-helmet";
+// import {Helmet} from "react-helmet";
+import {Helmet, HelmetProvider} from "react-helmet-async";
 import {Header} from "../Header/Header";
 import "./Layout.scss";
 
 export const Layout = ({children, title}) => {
   return (
-    <div className="layout">
+    <HelmetProvider>
       <Helmet>
         <meta charSet="utf-8" />
         <title>Pegasus | {title}</title>
       </Helmet>
       <Header />
       {children}
-    </div>
+    </HelmetProvider>
   );
 };
